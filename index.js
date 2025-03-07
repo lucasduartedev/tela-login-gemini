5d16d4bcec1565f758e05784306e1bdffb6ccbbe
Lucas Duarte <lucasduartedev@gmail.com>
2025-03-07 15:43:51 -0300

    Implementação botaõ tema escuro/claro e pequenas alterações

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+const loginContainer = document.querySelector('.login-container');
+const formInputs = document.querySelectorAll('.form-group input');
+const themeLabel = document.querySelector('.theme-label');
+
+const caixaUsuario = document.getElementById('username')
+const caixaSenha = document.getElementById('password')
+
+
+themeToggle.addEventListener('change', () => {
+    body.classList.toggle('dark-mode');
+    loginContainer.classList.toggle('dark-mode');
+    formInputs.forEach(input => input.classList.toggle('dark-mode'));
+    
+    // if (body.classList.contains('dark-mode')) {
+    //     themeLabel.textContent = 'Tema Claro';
+    // } else {
+    //     themeLabel.textContent = 'Tema Escuro';
+    // }
+});
+        
+// Foco na caixa de usuário
+let focarCaixaUsuario = () => {
+    caixaUsuario.focus()
+}
+
+focarCaixaUsuario()
+
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Impede o envio padrão do formulário
 
@@ -13,4 +42,9 @@ document.getElementById('login-form').addEventListener('submit', function(event)
         messageDiv.textContent = 'Usuário ou senha incorretos.';
         messageDiv.className = 'message error';
     }
+    
+    // Limpar campo senha após tentar logar
+    caixaSenha.value = ''
+    focarCaixaUsuario()
+
 });
